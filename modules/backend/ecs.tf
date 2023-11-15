30051255a4386e4a61b755cfca81a8ee5a660657
Mark Cooper <mark.c.cooper@outlook.com>
2023-11-15 10:49:52 -0800

    Support placement strategies, resolves #20 (#21)
    Set default placement strategy to pack by memory. Containers will
    deployed onto instances that have memory available first, before
    scaling out.
    
    https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task-placement-strategies.html

--- modules/backend/ecs.tf
+++ modules/backend/ecs.tf
@@ -62,6 +62,14 @@ resource "aws_ecs_service" "this" {
     subnets          = local.subnets
   }
 
+  dynamic "ordered_placement_strategy" {
+    for_each = local.placement_strategies
+    content {
+      field = ordered_placement_strategy.value.field
+      type  = ordered_placement_strategy.value.type
+    }
+  }
+
   tags = local.tags
 }
 
