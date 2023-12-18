0027e786923b570a4a1140268eef02ea6763216f
mglyrasis <129785358+mglyrasis@users.noreply.github.com>
2023-12-18 11:51:21 -0700

    🐛 More debugging information

--- modules/backend/outputs.tf
+++ modules/backend/outputs.tf
@@ -2,3 +2,16 @@ output "hostnames" {
   description = "The hostname(s) expected by the load balancer for the generated routes."
   value       = local.hostnames
 }
+
+# Debugging purposes
+output "full_hostname" {
+  value = local.full_hostname
+}
+
+output "host_with_alias" {
+  value = local.host_with_alias
+}
+
+output "manual_host_with_alias" {
+  value = local.is_zone_alias ? "${local.zone_alias}.${local.zone}" : local.zone
+}
