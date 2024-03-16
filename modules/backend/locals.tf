0c9c732a714213b1735ce299388847d3974eef4b
Mark Cooper <mark.c.cooper@outlook.com>
2024-03-15 20:12:46 -0700

    Just some reformatting for readability

--- modules/backend/locals.tf
+++ modules/backend/locals.tf
@@ -5,7 +5,7 @@ locals {
   cluster_id                = var.cluster_id
   collectionspace_memory_mb = var.collectionspace_memory_mb
   container_port            = var.container_port
-  cpu                       = local.capacity_provider == "EC2" ? null : var.cpu
+  cpu                       = var.cpu
   create_db                 = var.create_db
   cspace_memory             = var.collectionspace_memory_mb
   cspace_ui_build           = var.cspace_ui_build
@@ -16,23 +16,39 @@ locals {
   health_check_attempts     = var.health_check_attempts
   health_check_interval     = var.health_check_interval
   health_check_path         = var.health_check_path
+  host_headers              = concat([local.full_hostname], local.extra_hosts)
+  host_with_alias           = length(local.zone_alias) > 0 ? "${local.zone_alias}.${local.zone}" : local.zone
+  img                       = var.img
+  img_tag                   = split(":", var.img)[1]
+  img_repository            = regex("/(.*):", var.img)[0]
+  instance_count            = var.instances
+  listener_arn              = var.listener_arn
+  name                      = var.name
+  pathname_override         = var.pathname_override
+  placement_strategies      = local.capacity_provider == "EC2" ? var.placement_strategies : {}
+  profiles                  = var.profiles
+  requires_compatibilities  = var.requires_compatibilities
+  resource_prefix           = (length(local.zone_alias) == 0 || local.name == local.zone_alias) ? local.name : "${local.name}${local.zone_alias}"
+  route_prefix              = (length(local.zone_alias) == 0 || local.name == local.zone_alias) ? local.name : "${local.name}.${local.zone_alias}"
+  security_group_id         = var.security_group_id
+  sns_topic_arn             = var.sns_topic_arn
+  subdomain_override        = var.subdomain_override
+  subnets                   = var.subnets
+  swap_size                 = 1024
+  tags                      = var.tags
+  task_memory_buffer_mb     = var.task_memory_buffer_mb
+  template_path             = "${path.module}/task-definition/app.json.tpl"
+  timezone                  = var.timezone
+  vpc_id                    = var.vpc_id
+  zone                      = var.zone
+  zone_alias                = var.zone_alias
+
+  # derive hostnames from profiles (used as outputs for creating dns records)
   hostnames = length(local.profiles) == 1 ? [local.full_hostname] : [
     for profile in local.profiles : "${profile}.${local.host_with_alias}"
   ]
-  host_headers             = concat([local.full_hostname], local.extra_hosts)
-  host_with_alias          = length(local.zone_alias) > 0 ? "${local.zone_alias}.${local.zone}" : local.zone
-  img                      = var.img
-  img_tag                  = split(":", var.img)[1]
-  img_repository           = regex("/(.*):", var.img)[0]
-  instance_count           = var.instances
-  listener_arn             = var.listener_arn
-  name                     = var.name
-  pathname_override        = var.pathname_override
-  placement_strategies     = local.capacity_provider == "EC2" ? var.placement_strategies : {}
-  profiles                 = var.profiles
-  requires_compatibilities = var.requires_compatibilities
-  resource_prefix          = (length(local.zone_alias) == 0 || local.name == local.zone_alias) ? local.name : "${local.name}${local.zone_alias}"
-  route_prefix             = (length(local.zone_alias) == 0 || local.name == local.zone_alias) ? local.name : "${local.name}.${local.zone_alias}"
+
+  # derive routes from profiles (used to create listener rules)
   routes = length(local.profiles) == 1 ? [{
     name = local.route_prefix
     host = local.full_hostname
@@ -44,20 +60,10 @@ locals {
       path = "/cspace/${profile}/login"
     }
   ]
-  security_group_id     = var.security_group_id
-  sns_topic_arn         = var.sns_topic_arn
-  subdomain_override    = var.subdomain_override
-  subnets               = var.subnets
-  swap_size             = 1024
-  tags                  = var.tags
-  task_memory_buffer_mb = var.task_memory_buffer_mb
+
+  # determine memory (task definition hard limit)
   task_memory_mb = max(
     var.task_memory_mb,
     local.collectionspace_memory_mb + local.task_memory_buffer_mb
   )
-  template_path = "${path.module}/task-definition/app.json.tpl"
-  timezone      = var.timezone
-  vpc_id        = var.vpc_id
-  zone          = var.zone
-  zone_alias    = var.zone_alias
 }
